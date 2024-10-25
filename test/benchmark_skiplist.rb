fc9536b85583c811694b82d479f0b95b004eff5d
vxd7 <vxd732@protonmail.com>
2024-10-25 19:15:12 +0500

    feat(skiplist-deletion): benchmark

--- test/benchmark_skiplist.rb
+++ test/benchmark_skiplist.rb
@@ -3,56 +3,27 @@ require 'minitest/benchmark'
 
 class BenchmarkSkiplist < Minitest::Benchmark
   include Skiplist::LevelNumberGenerators
+  include SkiplistFillingHelpers
 
-  RANGE_MAX = 30000
+  TOLERANCE = 0.90
 
   def self.bench_range
-    (20000..RANGE_MAX).step(100)
+    (20_000..30_000).step(100)
   end
 
   def setup
-    @skiplists = {}
-    @elems = generate_random_values(RANGE_MAX).to_a
+    @skiplists, @elements = setup_benchmark_skiplists(self.class.bench_range)
+  end
 
-    gen = InverseTransformGeometric.new(
-      max_level: Skiplist::DEFAULT_MAX_LEVEL,
-      p_value: Skiplist::DEFAULT_P_VALUE
-    )
-    entropy = Array.new(100) { gen.call }.cycle
-
-    level_number_generator = SimpleDeterministic.new(entropy)
-    @ranges = self.class.bench_range.each do |range|
-      puts "Setup range: #{range}"
-      range_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-      skiplist = Skiplist.new
-      skiplist.level_number_generator = level_number_generator
-
-      @elems.take(range).each do |elem|
-        skiplist[elem] = elem
-      end
-      range_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-      puts "Finished in #{range_end - range_start} seconds\n"
-
-      @skiplists[range] = skiplist
+  def bench_skiplist_insertion
+    assert_performance_logarithmic(TOLERANCE) do |n|
+      4000.times { @skiplists[n][@elements.sample] = 'abacaba' }
     end
   end
 
-  def bench_skip_list_insertion
-    assert_performance_logarithmic(0.90) do |n|
-      4000.times { @skiplists[n][@elems.sample] = 'abacaba' }
+  def bench_skiplist_deletion
+    assert_performance_logarithmic(TOLERANCE) do |n|
+      4000.times { @skiplists[n].delete(@elements.sample) }
     end
-  end
-
-  private
-
-  def generate_random_values(size)
-    elems = Set.new
-    loop do
-      break if elems.size >= size
-
-      elems << rand(500 * size)
-    end
-
-    elems
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,15 +21,56 @@ module SkiplistFillingHelpers
   def fill_skiplist(skiplist, size)
     return rand(500).tap { |n| skiplist[n] = n } if size == 1
 
-    elems = Set.new
-    loop do
-      break if elems.size >= size
-
-      elems << rand(500 * size)
-    end
-
-    elems.each do |elem|
+    rand_range(size).each do |elem|
       skiplist[elem] = elem
     end
+  end
+
+  def rand_range(size)
+    range_start = rand(500 * size)
+    range_end = range_start + size
+
+    (range_start...range_end).to_a.shuffle
+  end
+
+  def setup_benchmark_skiplists(sequence)
+    level_numbers = geometric_distribution(100)
+    elements = rand_range(sequence.end)
+    skiplists = {}
+
+    sequence.each do |num_elems|
+      skiplists[num_elems] = setup_benchmark_skiplist(
+        level_numbers,
+        elements.take(num_elems)
+      )
+    end
+
+    [skiplists, elements]
+  end
+
+  def setup_benchmark_skiplist(level_numbers, elements)
+    skiplist = Skiplist.new
+
+    gen = Skiplist::LevelNumberGenerators::SimpleDeterministic.new(level_numbers.cycle)
+    skiplist.level_number_generator = gen
+
+    puts "Fill skiplist with #{elements.size} elements"
+    range_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+    elements.each { |element| skiplist[element] = element }
+
+    range_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    puts "Finished in #{range_end - range_start} seconds\n"
+
+    skiplist
+  end
+
+  def geometric_distribution(size)
+    gen = Skiplist::LevelNumberGenerators::InverseTransformGeometric.new(
+      max_level: Skiplist::DEFAULT_MAX_LEVEL,
+      p_value: Skiplist::DEFAULT_P_VALUE
+    )
+
+    Array.new(size) { gen.call }
   end
 end
