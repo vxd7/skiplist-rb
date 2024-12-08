66d40d978ed0b027b797a7293a3db1eb27c51e38
vxd7 <vxd732@protonmail.com>
2024-12-08 21:40:34 +0500

    refactor(skiplist): move pretty printer to separate class

--- lib/skiplist.rb
+++ lib/skiplist.rb
@@ -3,6 +3,7 @@
 require_relative 'skiplist/version'
 
 require_relative 'skiplist/node'
+require_relative 'skiplist/pretty_printer'
 require_relative 'skiplist/level_number_generators/geometric'
 
 class Skiplist
@@ -168,33 +169,15 @@ class Skiplist
     header.level
   end
 
+  def pretty_print
+    PrettyPrinter.new(self).call
+  end
+
   def header
     @header ||= Node.new(-Float::INFINITY, nil, default: finish)
   end
 
   def finish
     @finish ||= Node.new(Float::INFINITY, nil)
-  end
-
-  def pretty_print
-    rows = [header.traverse_level(0).to_a[1...-1]]
-    rows[0].each do |element|
-      lvl = element.level - 1
-      (1..lvl).each do |i|
-        rows[i] ||= []
-        rows[i] << element
-      end
-
-      ((lvl + 1)..header.level).each do |i|
-        rows[i] ||= []
-        rows[i] << nil
-      end
-    end
-
-    rows.map.with_index do |e, i|
-      next if e.compact.empty?
-
-      ["L#{i}:", 'H', *(e.map { |ee| ee ? ee.key : nil }), 'F'].join("\t")
-    end.compact.join("\n")
   end
 end
